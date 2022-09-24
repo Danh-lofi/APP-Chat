470ed78d9ab22089c043927dfdfcc31a9c32c216
danh <thdanh2001@gmail.com>
2022-09-24 13:44:23 +0700

    .

---
+++ App/app-reactjs/src/pages/Home/login/Login.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Login = (props) => {
+  return (
+    <div>
+      <button onClick={props.onClick}>register</button>
+    </div>
+  );
+};
+
+export default Login;
