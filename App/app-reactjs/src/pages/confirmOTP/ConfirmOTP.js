bd89f760d2757d935560ee879a0f7d68b5c9cb51
danh <thdanh2001@gmail.com>
2022-10-23 15:36:29 +0700

    Firebase

--- App/app-reactjs/src/pages/confirmOTP/ConfirmOTP.js
+++ App/app-reactjs/src/pages/confirmOTP/ConfirmOTP.js
@@ -4,40 +4,42 @@ import ButtonAuthen from "../../components/button/ButtonAuthen";
 import InputAuthen from "../../components/input/InputAuthen";
 import "./confirmOTP.scss";
 
-const ConfirmOTP = () => {
-    const [OTP, setOTP] = useState("");
-    const changeOTPHandle = (value) => {
-        setOTP(value);
-      };
-    const submitHandle = () => {
-        console.log(OTP);
-    }
-    return (
-        <div className="confirmOTP_container">
-            <div className="confirmOTP__title">
-                <h3 className="confirmOTP__title__main">Xác nhận mã OTP</h3>
-                <p className="confirmOTP__title__sub">Mã xác thực đã được gửi về số điện thoại của bạn. Vui lòng xác thực để tiếp tục.</p>
-            </div>
-            <div className="confirmOTP__input">
-                <InputAuthen
-                    label="Mã xác thực"
-                    type="text"
-                    placeholder="Nhập mã OTP"
-                    onInput={changeOTPHandle}
-                />
-            </div>
-            
-            <div className="button_authen__wrapper">
-                <ButtonAuthen onClick={submitHandle} content="Xác nhận" />
-            </div>
-            <div className="confirmOTP__resend__link">
-                <p>Không nhận được mã ? </p>
-                <Link to="/confirmOTP" className="confirmOTP__resend__link__text">
-                    Gửi lại
-                </Link>
-            </div>
-            
-        </div>
-    )
-}
+const ConfirmOTP = (props) => {
+  const [OTP, setOTP] = useState("");
+  const changeOTPHandle = (value) => {
+    setOTP(value);
+  };
+  const submitHandle = () => {
+    props.onSubmit(OTP);
+  };
+  return (
+    <div className="confirmOTP_container">
+      <div className="confirmOTP__title">
+        <h3 className="confirmOTP__title__main">Xác nhận mã OTP</h3>
+        <p className="confirmOTP__title__sub">
+          Mã xác thực đã được gửi về số điện thoại của bạn. Vui lòng xác thực để
+          tiếp tục.
+        </p>
+      </div>
+      <div className="confirmOTP__input">
+        <InputAuthen
+          label="Mã xác thực"
+          type="text"
+          placeholder="Nhập mã OTP"
+          onInput={changeOTPHandle}
+        />
+      </div>
+
+      <div className="button_authen__wrapper">
+        <ButtonAuthen onClick={submitHandle} content="Xác nhận" />
+      </div>
+      <div className="confirmOTP__resend__link">
+        <p>Không nhận được mã ? </p>
+        <Link to="/confirmOTP" className="confirmOTP__resend__link__text">
+          Gửi lại
+        </Link>
+      </div>
+    </div>
+  );
+};
 export default ConfirmOTP;
