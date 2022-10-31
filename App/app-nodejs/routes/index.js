84a00e295707843a93ba91fdc0a5aed4a2867401
danh <thdanh2001@gmail.com>
2022-10-31 15:26:22 +0700

    .

--- App/app-nodejs/routes/index.js
+++ App/app-nodejs/routes/index.js
@@ -2,7 +2,7 @@ import routerAuth from "./Auth.js";
 import routerChat from "./Chat.js";
 import routerMessage from "./Message.js";
 function route(app) {
-  app.use("/auth", routerAuth);
+  app.use("/", routerAuth);
   app.use("/chat", routerChat);
   app.use("/message", routerMessage);
 }
