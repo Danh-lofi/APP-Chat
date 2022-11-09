016c06421dff7afb052f11ed3be5b233a59896e0
Vu Nguyen <hvudtkt@gmail.com>
2022-11-09 20:41:37 +0700

    "xoa bb, tim bb"

--- App/app-nodejs/app/controllers/FriendController.js
+++ App/app-nodejs/app/controllers/FriendController.js
@@ -26,7 +26,26 @@ const FriendController = {
     }
     res.status(200).json({ listFriend });
   },
-
+  deleteFriend: async (req,res) => {
+    const user = req.user;
+    const meId = user._id
+    const friendId = req.body.friendId;
+    try {
+       await UserModel.findOneAndUpdate({_id: meId},{ $pull: { friends: {id: friendId} } });
+       await UserModel.findOneAndUpdate({_id: friendId},{ $pull: { friends: {id: meId} } });
+    } catch (error) {
+      console.log("loi");
+    }
+  },
+  getUserByUsername: async (req,res) =>{
+    const username = req.params.username;
+    try {
+      const user = await UserModel.findOne({username: username});
+      res.send(user)
+    } catch (error) {
+      res.send("khong tim thay user nay")
+    }
+  },
 };
 
 export default FriendController;
--- App/app-nodejs/routes/FriendRoute.js
+++ App/app-nodejs/routes/FriendRoute.js
@@ -5,6 +5,8 @@ import authMiddleware from "../app/middleware/authMiddleware.js";
 const routerFriend = express.Router();
 
 routerFriend.get("/", authMiddleware.isAuth, FriendController.getAllFriend);
+routerFriend.get("/:username", FriendController.getUserByUsername);
+routerFriend.post("/deleteFriend", authMiddleware.isAuth, FriendController.deleteFriend);
 
 
 export default routerFriend;
